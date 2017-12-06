d48a46b137438f6a9ff675cdb3a6244b16389db3
edgar615 <edgar615@gmail.com>
2017-12-06 13:52:33 +0800

    修改名称

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,16 +1,14 @@
 <template>
     <div>
-        <h1>element-ui-admin</h1>
+        <h1>vue-admin</h1>
         <p>基于 element-ui 的单页面后台管理项目模版</p>
+        <router-link to="/layout"> <el-button type="text">访问</el-button></router-link>
         <h2>技术栈</h2>
         <ul>
             <li>vue</li>
             <li>vue-resource</li>
             <li>vue-router</li>
             <li>element-ui</li>
-            <li>基于路由配置的导航菜单</li>
-            <li>基于路由配置的面包屑</li>
-            <li>按路由异步加载</li>
         </ul>
     </div>
 </template>
