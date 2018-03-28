8c08351278cb738e34149c22377eac1be1667b14
Edgar <edgar615@gmail.com>
2018-03-28 23:15:38 +0800

    两中登陆页

--- src/views/login/login3.vue
+++ src/views/login/login3.vue
@@ -1,21 +1,20 @@
 <template>
   <section class="hero is-success is-fullheight">
     <div class="hero-body">
-      <div class="container">
+      <div class="container has-text-centered">
         <div class="column is-4 is-offset-4">
-         <!-- <p class="subtitle has-text-grey">登录</p>-->
+          <h3 class="title has-text-white">XXX综合管理平台</h3>
           <div class="box">
-            <h3 class="title is-1 has-text-centered">{{$appName}}</h3>
-            <!--<figure class="avatar">
+            <figure class="avatar">
               <img src="https://placehold.it/128x128">
-            </figure>-->
-            <form>
+            </figure>
+            <form class="has-text-left">
               <div class="field">
                 <label class="label">用户名</label>
                 <div class="control" :class="{'has-icons-right': errors.has('username') }" >
                   <input name="username" v-validate="'required'" class="is-medium input" :class="{'is-danger': errors.has('username') }" type="text" placeholder="用户名"
                          data-vv-as="用户名" v-model="username">
-                    <span class="icon is-small is-right has-text-danger" v-show="errors.has('username')">
+                  <span class="icon is-small is-right has-text-danger" v-show="errors.has('username')">
                       <i class="fa fa-warning"></i>
                     </span>
                 </div>
@@ -32,10 +31,6 @@
                 </div>
                 <p v-show="errors.has('password')" class="help is-danger has-text-left">{{ errors.first('password')}}</p>
               </div>
-              <p class="has-text-grey">
-                <router-link to="/signup"> 注册</router-link> &nbsp;·&nbsp;
-                <router-link to="/signup"> 忘记密码</router-link>&nbsp;·&nbsp;
-              </p>
               <a class="button is-block is-info is-medium" style="font-size: 16px; height: 2.8em; line-height: 1.8em; "  @click="login" :disabled='errors.any()' :class="{'is-loading' : loading}">
                  <span class="icon">
                       <i class="fa fa-sign-in"></i>
@@ -43,9 +38,11 @@
                 登录
               </a>
             </form>
-
           </div>
-
+          <p class="has-text-white">
+            <router-link class="has-text-white" to="/signup"> 注册</router-link> &nbsp;·&nbsp;
+            <router-link class="has-text-white" to="/signup"> 忘记密码</router-link>&nbsp;·&nbsp;
+          </p>
         </div>
       </div>
     </div>
@@ -90,22 +87,14 @@
     font-weight: 300;
   }
   .hero.is-success {
-    background-color: #2269c2;
     background: url("../../assets/login_bg.jpg");
-    background-position: left top;
-    background-repeat: no-repeat;
-    background-size: 100%;
   }
   .hero .nav, .hero.is-success .nav {
     -webkit-box-shadow: none;
     box-shadow: none;
   }
   .box {
-    margin-top: 1rem;
-    padding: 30px 40px;
-  }
-  .label {
-    color: #333;
+    margin-top: 5rem;
   }
   .avatar {
     margin-top: -70px;
@@ -118,57 +107,4 @@
     -webkit-box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
     box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
   }
-  input {
-    font-weight: 300;
-  }
-  p {
-    font-weight: 300;
-    font-size: 14px;
-  }
-  p.subtitle {
-    padding-top: 1rem;
-  }
-
-  .field:not(:last-child) {
-    margin-bottom: 1.5rem;
-  }
-
-  .hero.is-success .title{
-    color: #333;
-    font-size: 16px;
-    margin-bottom: 30px;
-  }
-
-  .has-text-grey{
-    margin-top: 20px;
-    padding-bottom: 40px;
-  }
-
-  .has-text-grey a{
-    font-size: 13px;
-    color: #666;
-    background: none;
-    font-weight: normal;
-  }
-
-  .has-text-grey a:hover{
-    color: #2088ec;
-  }
-
- /* input{
-    background: #f4f4f4;
-    border: 1px solid #d2d2d2;
-    color: #666;
-    font-size: 14px;
-    box-shadow: none;
-  }
-
-  .input.is-medium, .taginput .is-medium.taginput-container.is-focusable, .textarea.is-medium {
-    font-size: 1.25rem;
-    font-size: 14px;
-    color: #666;
-    padding: 0 20px;
-    padding-right: 20px;
-    height: 3.2em;
-  }*/
 </style>
